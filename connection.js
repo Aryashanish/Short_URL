a042a6d250398860e4be54cc9e3528d63bcc0775
Arya_Shanish <shanishacharya@gmail.com>
2023-11-16 15:32:22 +0530

    final commit

--- connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-//connection
-async function connectMongodb(url) {
-    return mongoose.connect(url);
-}
-
-module.exports = { connectMongodb, };
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {handleNewuser,handleUserlogin} = require("../controllers/user");
-
-router.post("/", handleNewuser);
-
-router.post("/login", handleUserlogin);
-
-module.exports = router;
